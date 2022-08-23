aef8197985970d2fc53e0951756e627fade28b0d
Ricardo Torres <ricardo.torres0426@gmail.com>
2022-08-22 22:56:36 -0500

    Se agrego la estructura de la orden de compra

---
+++ src/components/Order.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Order = () => {
+  return (
+    <div className="order">
+            <p>
+              <span>04/26/22</span>
+              <span>6 articles</span>
+            </p>
+            <p>560.50</p>
+            <img src="./assets/icons/flechita.svg" alt="arrow"/>
+          </div>
+
+  );
+}
+
+export default Order;
