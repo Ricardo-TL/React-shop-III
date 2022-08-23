4b5e9ec3639cca02e7686c9c5af726688049e818
Ricardo Torres <ricardo.torres0426@gmail.com>
2022-08-22 22:28:56 -0500

    Se añadio la estructura para la card de los productos

---
+++ src/components/OrderItems.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const OrderItems = () => {
+  return (
+    <div className="order-item">
+            <figure>
+              <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike"/>
+            </figure>
+            <p>Bike</p>
+            <p>50.00</p>
+          </div>
+  );
+}
+
+export default OrderItems;
