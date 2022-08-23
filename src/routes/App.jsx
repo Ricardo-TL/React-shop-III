430ff69a9c6b5a61d3a93a59906270df6fd6341d
Ricardo Torres <ricardo.torres0426@gmail.com>
2022-08-23 00:15:21 -0500

    Se movieron los archivos a otra carpeta por cuestiones de atomic design

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Layout from '../containers/Layout';
-import Login from '../containers/Login';
-import RecoveryPassword from '../containers/RecoveryPassword';
+import Login from '../pages/Login';
+import RecoveryPassword from '../pages/RecoveryPassword';
 import Home from '../pages/Home';
-import OrderItems from '../components/OrderItems';
-import ProductItem from '../components/ProductItem';
-import Order from '../components/Order';
-import ShoppingCartItem from '../components/ShoppingCartItem';
-import Menu from '../components/Menu';
-import ProductInfo from '../components/ProductInfo';
 import NotFound from '../pages/NotFound';
 import '../styles/global.css';
 
@@ -18,14 +12,9 @@ const App = () => {
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route exact path="/"  element={<Home/>}/>
+          <Route exact path="/home"  element={<Home/>}/>
           <Route exact path="/Login"  element={<Login/>}/>
           <Route exact path="/recovery-password"  element={<RecoveryPassword/>}/>
-          <Route exact path="/order-items"  element={<OrderItems/>}/>
-          <Route exact path="/product-items"  element={<ProductItem/>}/>
-          <Route exact path="/order"  element={<Order/>}/>
-          <Route exact path="/shopping-cart-item"  element={<ShoppingCartItem/>}/>
-          <Route exact path="/menu"  element={<Menu/>}/>
           <Route exact path="/product-info"  element={<ProductInfo/>}/>
           <Route path="*" element={<NotFound/>}/>
         </Routes>
