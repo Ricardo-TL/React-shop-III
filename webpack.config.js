3af08f71f1c86b7f2f6a7b4d2c199b8be6b50b38
Ricardo Torres <ricardo.torres0426@gmail.com>
2022-08-23 23:06:46 -0500

    Se agregaron las configuraciones a webpack para poder trabajr con los formatos de imagenes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugiin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugiin = require('mini-css-extract-plugin');
 
 
 module.exports = {
@@ -13,6 +13,13 @@ module.exports = {
   mode: 'development',
   resolve: {
     extensions: ['.js', '.jsx'],
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@containers': path.resolve(__dirname, 'src/containers'),
+      '@styles':path.resolve(__dirname,'src/styles/'),
+      '@icons':path.resolve(__dirname,'src/assets/icons/'),
+      '@logos':path.resolve(__dirname,'src/assets/logos/'),
+    }
   },
   module: {
     rules: [
@@ -30,6 +37,10 @@ module.exports = {
             loader: 'html-loader'
           }
         ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset'
       },
       {
         test: /\.(css|scss)$/,
