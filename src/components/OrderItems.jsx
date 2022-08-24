ef04350f56db358e49826c91a5d4eb3097bb1da7
Ricardo Torres <ricardo.torres0426@gmail.com>
2022-08-23 23:58:52 -0500

    Se cambió la url de los productos

--- src/components/OrderItems.jsx
+++ src/components/OrderItems.jsx
@@ -5,10 +5,10 @@ const OrderItems = () => {
   return (
     <div className="order-item">
             <figure>
-              <img src="https://www.pexels.com/es-es/foto/trafico-gente-roto-coche-12128383/" alt="plane"/>
+              <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike"/>
             </figure>
-            <p>plane</p>
-            <p>5000</p>
+            <p>Bike</p>
+            <p>50.00</p>
           </div>
   );
 }
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -5,11 +5,11 @@ import addToCartbutton from '@icons/bt_add_to_cart.svg'
 
 const ProductItem = () => {
   return (<div className="ProductItem">
-              <img src="https://www.pexels.com/es-es/foto/trafico-gente-roto-coche-12128383/" alt="plane" />
+              <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike" />
               <div className="product-info">
                 <div>
                   <p>$120,00</p>
-                  <p>plane</p>
+                  <p>Bike</p>
                 </div>
                 <figure>
                   <img src={addToCartbutton} alt="" />
