4a351422de05b360424a32421e3b51b90cfe814d
Ricardo Torres <ricardo.torres0426@gmail.com>
2022-08-30 20:38:07 -0500

    Se añadio la estructura para trabajar los formularios de forma mas segura con useRef

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../styles/Login.scss'
 
 const Login = () => {
+  const form = useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(form.current);
+    const data = {
+      userName: formData.get('email'),
+      password: formData.get('password')
+    }
+    console.log(data);
+  }
+
   return (
     <div className="login">
       <div className="form-container">
         <img src="./assets/logos/logo_yard_sale.svg" alt="logo" className="logo"/>
-        <form action="/" className="form">
+        <form action="/" className="form" ref={form}>
           <label for="email">Email address</label>
-          <input type="text" id="email" placeholder="Email" className="input input-email"/>
+          <input type="text" name="email" placeholder="Email" className="input input-email"/>
           <label for="password">Password</label>
-          <input type="password" id="password" placeholder="*********" className="input input-password"/>
-          <input type="submit" value="Log in" className="primary-button login-button"/>
+          <input type="password" name="password" placeholder="*********" className="input input-password"/>
+          <button
+          onClick={handleSubmit}
+          className="primary-button login-button">
+          Log in
+          </button>
           <a href="/">Forgot my password</a>
           <button className="secondary-button signup-button">Sign up</button>
         </form>
